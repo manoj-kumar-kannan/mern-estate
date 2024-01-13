13adf7fb035b80b40adb7050bb50f790b4d07ff0
manoj kumar <manoj-kumar.kannan@capgemini.com>
2024-01-13 20:27:44 +0530

    install router package and add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
-  return <div className='text-cyan-400'>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
