b216ed0b8879d6601d43dfda9f15b3b908df7cbf
manoj kumar <manoj-kumar.kannan@capgemini.com>
2024-01-14 18:51:49 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected !!'))
+  .catch((err) => console.log(err));
 
 const PORT = 8000;
 
