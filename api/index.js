891a56d04408740a3420fcd5647f7af7e2ff42b0
manoj kumar <manoj-kumar.kannan@capgemini.com>
2024-01-14 23:12:21 +0530

    add auth route and controller

--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
+import authRoute from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -19,3 +22,4 @@ app.listen(PORT, () => {
 });
 
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
