5984982c075b7ad50802d4863f24afe8b754990f
manoj kumar <manoj-kumar.kannan@capgemini.com>
2024-01-14 23:30:36 +0530

    add bcrypt and error handler to api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,14 @@
 import User from '../models/User.model.js';
+import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json(newUser);
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
 };
