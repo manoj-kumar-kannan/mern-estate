adcfc68822e51fd9a85c95c3e72117e9b0809507
manoj kumar <manoj-kumar.kannan@capgemini.com>
2024-01-14 19:18:26 +0530

    add route and controller for user

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoute from './routes/user.route.js';
 
 dotenv.config();
 
@@ -16,3 +17,5 @@ const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server Started at: ${PORT} !!!`);
 });
+
+app.use('/api/user', userRoute);
