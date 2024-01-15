35d638bdc63fc285217319c5a0d8ba04328a12af
manoj kumar <manoj-kumar.kannan@capgemini.com>
2024-01-15 16:09:31 +0530

    add signin route and jwt package

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-import User from '../models/User.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.model.js';
+import { errorHandler } from '../util/error.js';
 
 export const signup = async (req, res, next) => {
   try {
@@ -8,6 +10,30 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, 'Invalid Credentials !!!'));
+
+    const payload = {
+      id: validUser._id,
+    };
+    const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET);
+
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    res
+      .cookie('accessToken', accessToken, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
